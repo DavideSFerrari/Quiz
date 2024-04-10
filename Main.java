7bb964e27ff1fa8119094693f37d57fb9fd92a23
davide ferrari <dferrari722@gmail.com>
2024-04-10 20:38:36 +0200

    Definition of the conditions for the game

--- Main.java
+++ Main.java
@@ -1,3 +1,15 @@
+// Define admin.
+// Define the player/-s.
+// Define the conditions were the players can start the game or not.
+// Define the quiz structure.
+// Define the turn conditions.
+// Define the rotation conditions.
+// Define the points cumulation for each rotation.
+// Define the final points.
+// Define the conditions for winning the whole game.
+
+
+
 public class Main
 {
 
