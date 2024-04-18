cc74ec4301f59c8553195a10ceca8ed09e95767d
davide ferrari <dferrari722@gmail.com>
2024-04-18 07:43:49 +0200

    Definition of classes and methods.

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
-// Create a class for Question, and with all its internal implications
+// Create a class for Question, and with all its internal implications:
+    // Instance variables (Id, question, answer)
+    // Constructor
+    // Getters and setters
 // Create a class which applies the Question objects, with its internal implications
+    // Creation of the instance variables data
+// Main
+    // Execution 
 
-import Player.Player;
-import Quiz.Quiz;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        System.out.println("Hello!");
+        QuestionService questionService = new QuestionService();
+        questionService.displayQuestion();
         
     }
 
