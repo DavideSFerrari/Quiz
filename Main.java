8e91ff895a32bcf4cc0fc0e6fbb0e658ed60249a
davide ferrari <dferrari722@gmail.com>
2024-04-18 20:04:06 +0200

    Creation of the Main class and of it method.

--- Main.java
+++ Main.java
@@ -1,4 +1,13 @@
-
+// Create class Main
+    // Create void method and test it.
+// Create a Quiz project
+    // Class for questions: Question
+        // Definition of the questions traits;
+        // Definition of its basic methods, in order to set and access the info (Getters and setters).
+    // Class for question implementation: QuestionService
+        // Use of the question (Question obj):
+            // Creation of the Question obj;
+            // Creation for a method which implements the object.
 
 
 
@@ -8,8 +17,6 @@ public class Main
     public static void main(String[] args)
     {
         
-
-
     }
 
 }
