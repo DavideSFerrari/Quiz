c28ea85a21e14954f0d2dc0a787155f8966eb3ab
davide ferrari <dferrari722@gmail.com>
2024-04-18 19:54:38 +0200

    Restarting project from scratch. (2)

--- Main.java
+++ Main.java
@@ -1,21 +1,15 @@
-// Create a class for Question, and with all its internal implications:
-    // Instance variables (Id, question, answer)
-    // Constructor
-    // Getters and setters
-// Create a class which applies the Question objects, with its internal implications
-    // Creation of the instance variables data
-// Main
-    // Execution 
+
+
+
 
 
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello!");
-        QuestionService questionService = new QuestionService();
-        questionService.displayQuestion();
         
+
+
     }
 
 }
