0c9c888fd347fabf76a78f18088b15b9321c8b3a
davide ferrari <dferrari722@gmail.com>
2024-04-25 12:22:23 +0200

    Creation of user input

--- QuestionService.java
+++ QuestionService.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class QuestionService 
 {
 
     Question question[] = new Question[3];
+    Scanner prompt = new Scanner(System.in);
+    String answer[] = new String[3];
 
     public QuestionService()
     {
@@ -12,6 +16,7 @@ public class QuestionService
 
     public void displayQuestion()
     {
+        int i = 0;
         for (Question q : question)
         {
             System.out.println("Question " + q.getId() + ": " + question[0].getQuestion());
@@ -20,10 +25,12 @@ public class QuestionService
             System.out.println(q.getOpt2());
             System.out.println(q.getOpt3());
             System.out.println(q.getOpt4());
-            System.out.println(q.getOpt1());
-            System.out.println(q.getOpt2());
-            System.out.println(q.getOpt3());
-            System.out.println(q.getOpt4());
+            System.out.println("Give the right answer:");
+            answer[i] = prompt.nextLine();
+            i++;
+        }
+        for (Question q : question)
+        {
             System.out.println("Correct answer: "+ q.getAnswer());
         }
     }
